76e21c919f134f3c94810f8feffc96eb62cae505
urushan78 <urushaniroula@gmail.com>
2024-07-10 16:18:55 +0545

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='relative'>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000000" fillOpacity="1" d="M0,192L1440,64L1440,320L0,320Z"></path></svg>
 
-    <div className="info absolute right-1/2 top-1/2 text-white sm:right-[40%] sm:top-[40%]">
+    <div className="info absolute right-1/2 top-1/2 text-white sm:right-[40%] sm:top-[40%] py-2 ">
       <h1>Developed By</h1>
       <p>Urusha Niroula</p>
     </div>
